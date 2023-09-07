afe1fda2e2cfa1cf8d15bc19c5c77bd9c0b6903a
sara-saidi <saidisarra782022@gmail.com>
2023-09-07 22:44:25 +0100

    router

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './Components/App/App.js';
 import {BrowserRouter} from "react-router-dom";
-import App from './components/App/App';
 
 ReactDOM.render(<BrowserRouter><App/></BrowserRouter>,document.getElementById('root'));
